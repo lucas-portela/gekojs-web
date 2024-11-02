d4a15b0bd49265e20b9fa45bbe5fa3717479ed2e
Lucas Portela <contact@lucasportela.dev>
2024-11-02 01:39:56 -0300

    Remove dist from git

--- dist/errors.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class AddingRouteAfterListeningError extends Error {
-    constructor();
-}
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./web-server";
-export * from "./genes";
-export * from "./types";
-export * from "./errors";
